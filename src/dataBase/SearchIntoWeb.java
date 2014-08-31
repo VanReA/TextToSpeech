df28c6d8b64c892a75a4088442770840b52bd26b
VaNReA <vanrea3579@gmail.com>
2014-08-31 23:37:23 +0300

    Update Search Engine. TODO: Try Hmmm.java
    Signed-off-by: VaNReA <vanrea3579@gmail.com>

--- src/dataBase/SearchIntoWeb.java
+++ src/dataBase/SearchIntoWeb.java
@@ -13,16 +13,28 @@ public class SearchIntoWeb {
 
 	public static void main(String[] args) throws IOException {
 
-		URL url = new URL("http://www.imdb.com/title/tt1905041/?ref_=nv_sr_3");
+		String[] allGenres = { "Action", "Adventure", "Animation",
+			    "Biography", "Comedy", "Crime", "Documentary", "Drama",
+			    "Family", "Fantasy", "Film-Noir", "History", "Horror",
+			    "Music", "Musical", "Mystery", "Romance", "Sci-Fi",
+			    "Short", "Sport", "Thriller", "War", "Western"} ;
+
+//		URL url = new URL("http://www.imdb.com/title/tt1905041/?ref_=nv_sr_3");
+		URL url = new URL("http://www.imdb.com/title/tt0068646/?ref_=chttp_tt_2");
 		URLConnection con = url.openConnection();
 		InputStream in = con.getInputStream();
 		String encoding = con.getContentEncoding();
 		encoding = encoding == null ? "UTF-8" : encoding;
 		String body = IOUtils.toString(in, encoding);
-//		System.out.println(body);
+		String restHTML = (String) body.subSequence(body.indexOf("itemprop=\"genre\""), body.length());
+		String finalHTML4Search = "";
+		finalHTML4Search = (String) restHTML.subSequence(0, restHTML.indexOf("</div>"));
 		
-//		System.out.println(body.indexOf("itemprop=\"genre\""));
-		System.out.println(body.subSequence(body.indexOf("itemprop=\"genre\""), body.indexOf("itemprop=\"genre\"")+300));
+		for(String genre : allGenres){
+			if(finalHTML4Search.contains(genre))
+				System.out.println(genre);
+		}
+//		System.out.println(finalHTML4Search);
 		
 	}
 
